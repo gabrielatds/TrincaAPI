a7a10663634c1665726d839edb035ccf007e2ee2
Gabriel <gabriel_3404@hotmail.com>
2020-09-29 13:58:39 -0300

    Swagger configurado

--- API/ChurrasAPI/Startup.cs
+++ API/ChurrasAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using ChurrasAPI.Interfaces;
 using ChurrasAPI.Services;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace ChurrasAPI
 {
@@ -31,6 +34,30 @@ namespace ChurrasAPI
             services.AddDbContext<ApplicationContext>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ChurrasConnectionString")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Lançamento de Horas API",
+                    Description = "Desafio Back-end Luby Software",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Gabriel Alves",
+                        Url = new Uri("https://github.com/gabrielatds"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
+
             services.AddScoped<SeedingService>();
             services.AddScoped<IChurrascoService, ChurrascoService>();
         }
@@ -38,6 +65,12 @@ namespace ChurrasAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
