0f0f9b1eeea0c1600bb37cdb36ddc39e9132fe43
Gabriel <gabriel_3404@hotmail.com>
2020-09-28 21:03:09 -0300

    Projeto criado. Models Criados

---
+++ API/ChurrasAPI/Models/Churrasco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChurrasAPI.Models
+{
+    public class Churrasco
+    {
+        public DateTime Data { get; set; }
+        public string Descricao { get; set; }
+        public string ObsAdicional { get; set; }
+        public List<Participante> Participantes { get; set; } = new List<Participante>();
+    }
+}
---
+++ API/ChurrasAPI/Models/Participante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChurrasAPI.Models
+{
+    public class Participante
+    {
+        public string Nome { get; set; }
+        public double Contribuicao { get; set; }
+    }
+}
